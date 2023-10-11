dcfc0b46acdaa2fbae05481ad09be8c5fe588bd3
Rop Kelvin <kropchir@gmail.com>
2023-10-11 19:58:21 +0000

    deletes key in dic update

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
 def simple_delete(a_dictionary, key=""):
-    a_dictonary.pop(key, None)
-    return my_dict
+    if a_dictionary.get(key) is not None:
+        del a_dictionary[key]
+    return (a_dictionary)
