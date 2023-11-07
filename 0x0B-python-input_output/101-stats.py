f96bb339f25b58f543a14ea7f75c133bc178df55
Rop Kelvin <kropchir@gmail.com>
2023-11-07 16:49:59 +0000

    fixes

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -7,7 +7,6 @@ prints the following statistics:
     - Count of read status codes up to that point.
 """
 
-
 def print_stats(size, status_codes):
     """Print accumulated metrics.
 
@@ -19,13 +18,12 @@ def print_stats(size, status_codes):
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
 
-
-if __name__ == "__main__":
+if __name__ == "__main":
     import sys
 
     size = 0
     status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    valid_codes = {'200', '301', '400', '401', '403', '404', '405', '500'}
     count = 0
 
     try:
@@ -45,10 +43,12 @@ if __name__ == "__main__":
 
             try:
                 if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
+                    if line[-2] not in status_codes:
                         status_codes[line[-2]] = 1
                     else:
-                        pass
+                        status_codes[line[-2]] += 1
+            except (IndexError, ValueError):
+                pass
 
         print_stats(size, status_codes)
 
