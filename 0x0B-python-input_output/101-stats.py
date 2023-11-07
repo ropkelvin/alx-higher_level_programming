ca38abf48301820518e7f3ad0830ba7874e32c50
Rop Kelvin <kropchir@gmail.com>
2023-11-07 16:51:51 +0000

    fixes

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,7 +3,7 @@
 
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
-    - Total file size up to that point.
+    - Total file size to that point.
     - Count of read status codes up to that point.
 """
 
