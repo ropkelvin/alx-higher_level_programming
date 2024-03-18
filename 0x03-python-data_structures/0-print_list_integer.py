b94e029a5a82fade9a91a9615fff332329107adc
Rop Kelvin <kropchir@gmail.com>
2024-03-18 19:09:58 +0300

    prints elements in list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+'''
+    function that prints all integers of a list.
+'''
+
+
+def print_list_integer(my_list=[]):
+    for element in my_list:
+        print("{}".format(element))
