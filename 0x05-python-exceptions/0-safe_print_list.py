fdbf5fa841ed0e2be52e75de58a928ae1aaee17b
Rop Kelvin <kropchir@gmail.com>
2024-04-02 22:58:41 +0300

    exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    total = 0
+
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            total += 1
+
+    except IndexError:
+        pass
+    print("")
+    return (total)
