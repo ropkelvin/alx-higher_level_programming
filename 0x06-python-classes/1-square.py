5c5bc972cc16c303735148a60e0e9e69c9c69fe2
Rop Kelvin <kropchir@gmail.com>
2024-04-14 21:15:17 +0300

    Private instance attribute: size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' empty class Square that defines a square
+'''
+
+
+class Square:
+    '''Description for class square'''
+    def __init__(self, size):
+        self.__size = size
