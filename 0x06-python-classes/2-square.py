ae8ad3b3333c9d0c40e7ebc57bc8da3ff219ddbe
Rop Kelvin <kropchir@gmail.com>
2024-04-15 12:34:06 +0300

    defines a square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+A class Square that defines a square
+'''
+
+class Square:
+    '''
+    Description for class square
+    '''
+    def __init__(self, size = 0):
+        '''
+        Initializes a new Square object with a given size
+        Args:
+            size (int): The size of the square (default is 0)
+        '''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
