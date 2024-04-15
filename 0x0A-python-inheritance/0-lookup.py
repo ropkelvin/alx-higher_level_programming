ce9931c33a78805e7fe7c7f8d0d88e1a0a88e62f
Rop Kelvin <kropchir@gmail.com>
2024-04-15 18:53:13 +0300

    returns attributes and methods

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+def lookup(obj):
+    """
+    Function that returns a list of attributes and methods of the given object.
+
+    Parameters:
+    obj: Any object - The object for which need to be looked up.
+
+    Returns:
+    List - A list of strings representing the attributes and methods.
+    """
+    return dir(obj)
