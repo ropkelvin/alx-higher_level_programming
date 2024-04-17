8d403beed758529e74cbf6ef664a7f195a956b75
Rop Kelvin <kropchir@gmail.com>
2024-04-17 21:36:16 +0300

    reads atext

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+ function that reads a text file (UTF8) and prints it to stdout
+'''
+
+
+def read_file(filename=""):
+    """
+    Reads and prints the content of a file.
+
+    Args:
+        filename (str): The name of the file to be read.
+        Defaults to an empty string.
+
+    """
+    with open(filename, encoding='utf-8') as a_file:
+        print(a_file.read())
