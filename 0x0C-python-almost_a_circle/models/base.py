cd6a59b6f43a1b0e3f056e3af4500d6fe1677cea
Rop Kelvin <kropchir@gmail.com>
2024-04-21 23:15:55 +0300

    base update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines a base model class."""
+import json
 
 
 class Base:
@@ -29,3 +30,20 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        Converts a list of dictionaries to a JSON string.
+
+        If the input is None, it returns an empty JSON array string "[]".
+
+        Args:
+            list_dictionaries (list): A list of dictionaries to be converted.
+
+        Returns:
+            str: JSON string representation of the input list of dictionaries.
+        """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
