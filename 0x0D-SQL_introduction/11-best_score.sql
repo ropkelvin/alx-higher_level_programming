7cbd354ee5b5090ee60a7a1c17c05f74564f832f
Rop Kelvin <kropchir@gmail.com>
2024-05-22 14:36:50 +0300

    more sorting

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,4 @@
--- Select 'score' and 'name' from 'second_table' where 'score' is greater than or equal to 10
+-- Select 'score' and 'name' from 'second_table'
+--where 'score' is greater than or equal to 10
 --ordered by 'score' in descending order
 SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
