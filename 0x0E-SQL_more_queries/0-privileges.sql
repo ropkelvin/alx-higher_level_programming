ea7fd338e5fe19420fc4d84ff9d0006897a0cbb7
Rop Kelvin <kropchir@gmail.com>
2024-05-23 22:06:28 +0300

    list users privileges

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,8 @@
+-- This script lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
+
+-- Use the SHOW GRANTS command to display the privileges granted to a MySQL user
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- The above command will list all the privileges of the user 'user_0d_1' on 'localhost'
+
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- The above command will list all the privileges of the user 'user_0d_2' on 'localhost'
